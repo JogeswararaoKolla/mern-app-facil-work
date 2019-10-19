7eab5d44553242fc021f4f92da29bc6611e8de6e
Aaron <aaronmgardner1@gmail.com>
2019-10-19 13:00:36 -0700

    created material procurement component

--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import TimeSheet from './components/timesheet/timesheet';
+import MatProcure from './components/matprocure/matprocure';
 
 export default props => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/timesheet" component={ TimeSheet } />
+            <Route exact path="/matprocure" component={ MatProcure} />
         </Switch>
     </BrowserRouter>
   )
