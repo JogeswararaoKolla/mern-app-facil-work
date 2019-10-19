20392bd984fc57f5ff30faccd67a4194d341ff60
Aaron <aaronmgardner1@gmail.com>
2019-10-18 22:06:59 -0700

    Basic Bootstrap template for TimeSheet

---
+++ client/src/components/timesheet/timesheet.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import InfoBar from '../infobar/infobar';
+import NavbarHeader from '../navbar/navbar';
+
+class TimeSheet extends React.Component {
+
+    render(){
+        return(
+            <React.Fragment>
+                <NavbarHeader />
+                <InfoBar title="Time Sheet" />
+                <Container>
+                    <h3>Worker 1</h3>
+                    <Table striped bordered hover variant="dark">
+                        <thead>
+                            <tr>
+                            <th>Date</th>
+                            <th>Project</th>
+                            <th>Work Assigned</th>
+                            <th>Allotted Hours</th>
+                            <th>Worked Hours</th>
+                            <th>Current Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>10/11/2019</td>
+                                <td>Project 1</td>
+                                <td>Do template</td>
+                                <td>5</td>
+                                <td>7</td>
+                                <td>Pending</td>
+                            </tr>
+                            <tr>
+                                <td>10/15/2019</td>
+                                <td>Project 2</td>
+                                <td>Do template 2</td>
+                                <td>5</td>
+                                <td>7</td>
+                                <td>Completed</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                    <h3>Worker 2</h3>
+                    <Table striped bordered hover variant="dark">
+                        <thead>
+                            <tr>
+                            <th>Date</th>
+                            <th>Project</th>
+                            <th>Work Assigned</th>
+                            <th>Allotted Hours</th>
+                            <th>Worked Hours</th>
+                            <th>Current Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>10/11/2019</td>
+                                <td>Project 1</td>
+                                <td>Do template</td>
+                                <td>5</td>
+                                <td>7</td>
+                                <td>Pending</td>
+                            </tr>
+                            <tr>
+                                <td>10/15/2019</td>
+                                <td>Project 2</td>
+                                <td>Do template 2</td>
+                                <td>5</td>
+                                <td>7</td>
+                                <td>Completed</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Container>
+            </React.Fragment>
+        )
+    }
+}
+
+export default TimeSheet;
