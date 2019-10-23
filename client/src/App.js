883f74d7640a59b6d5e8920e775a3ede3ab0e4bf
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-10-22 21:35:16 -0700

    timesheet file

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,31 @@ import LoginForm from "./components/Signup-Login/login-form";
 import Signup from "./components/Signup-Login/sign-up";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarHeader from "./components/navbar/navbar";
+import TimeSheet from "./components/timesheet/timesheet.js";
+import TimeSheetForm from "./components/timesheet-form/timesheet-form.js";
 
 class App extends Component {
-    state = {
-        firstName: "",
-        role: ""
-    }
+  state = {
+    firstName: "",
+    role: ""
+  };
 
-    render() {
-        return (
-            <Router>
-                <NavbarHeader></NavbarHeader>
-                <Container>
-                    <Route exact path="/" component={LoginForm} />
-                    <Route exact path="/sign-up" component={Signup} />
-                    <Route exact path="/welcome" component={Welcome} />
-                </Container>
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <NavbarHeader></NavbarHeader>
+        <Container>
+          <Route exact path="/timesheet" component={TimeSheet} />
+          <Route exact path="/add-timesheet" component={TimeSheetForm} />
+          <Route exact path="/" component={LoginForm} />
+          <Route exact path="/sign-up" component={Signup} />
+          <Route exact path="/welcome" component={Welcome} />
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/components/Signup-Login/sign-up.js
+++ client/src/components/Signup-Login/sign-up.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 import "./style.css";
 
 class Signup extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             username: '',
             firstName: '',
