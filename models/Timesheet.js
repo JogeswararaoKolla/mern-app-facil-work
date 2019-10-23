0077dbf8809ac74396115e07b1d8d2690448736f
Robert Anderson <riandersons@cox.net>
2019-10-23 15:46:33 -0700

    deleted files to revert to test

--- models/Timesheet.js
+++
@@ -1,2 +0,0 @@
-username
-{project_name: "hello", work_assigned: "helo", start_date: "2019-10-14", end_date: "2019-10-18", hours_worked: "20"}
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const useRoutes = require("./userRoutes.js");
-
-router.use("/api/users/", useRoutes);
-
-module.exports = router;
