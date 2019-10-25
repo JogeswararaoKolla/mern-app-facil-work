8c36d483c0d5ba096b08cf70360f8d2093e3aa1e
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-10-24 20:36:09 -0700

    added the Manager projects views functional component using React hooks

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import TimeSheet from "./components/timesheet/timesheet.js";
 import TimeSheetForm from "./components/timesheet-form/timesheet-form.js";
 import ManagerInputForm from "./components/ManagerInput/index.js";
 import MatProcurmentForm from "./components/matprocurment-form/matprocurment-form";
+import Manager1 from "./components/Manager1/index.js";
 
 class App extends Component {
   state = {
@@ -24,11 +25,12 @@ class App extends Component {
         <Container>
           <Route exact path="/timesheet" component={TimeSheet} />
           <Route exact path="/add-timesheet" component={TimeSheetForm} />
-          <Route exact path="/add-material" component={ MatProcurmentForm} />
+          <Route exact path="/add-material" component={MatProcurmentForm} />
           <Route exact path="/manager-input" component={ManagerInputForm} />
           <Route exact path="/" component={LoginForm} />
           <Route exact path="/sign-up" component={Signup} />
           <Route exact path="/welcome" component={Welcome} />
+          <Route exact path="/projects" component={Manager1} />
         </Container>
       </Router>
     );
