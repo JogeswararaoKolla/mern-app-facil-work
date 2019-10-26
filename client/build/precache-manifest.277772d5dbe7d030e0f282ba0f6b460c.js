4eeb18c6438ef13627b81b27e9e83305cea20e8c
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-10-25 23:35:21 -0700

    added the latest build

--- client/build/precache-manifest.277772d5dbe7d030e0f282ba0f6b460c.js
+++ client/build/precache-manifest.277772d5dbe7d030e0f282ba0f6b460c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "02cb5f179d7abfe8098aa74dfc940c66",
+    "revision": "fce7f4235f33a42c43a56ddbdac3733f",
     "url": "/index.html"
   },
   {
-    "revision": "13076630be6c3913ae2c",
+    "revision": "a47b19d141243ce4138d",
     "url": "/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "13076630be6c3913ae2c",
-    "url": "/static/js/2.b27c5665.chunk.js"
+    "revision": "a47b19d141243ce4138d",
+    "url": "/static/js/2.06fe323b.chunk.js"
   },
   {
-    "revision": "586699f74fd477910339",
-    "url": "/static/js/main.35727dd8.chunk.js"
+    "revision": "199e504b1d519f8a02e9",
+    "url": "/static/js/main.633b0c59.chunk.js"
   },
   {
     "revision": "875d570ce1b6fc43afc6",
